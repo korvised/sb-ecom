312ae7b13482ebb75935eeded73c4291df37d789
Korvised YOTSAIY <kvsyotsaiy@gmail.com>
2024-12-26 09:39:46 +0700

    implement security

---
+++ src/main/java/com/ecommerce/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ecommerce.security.services;
+
+import com.ecommerce.model.User;
+import com.ecommerce.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
